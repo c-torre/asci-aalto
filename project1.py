295d4778f28684828439cec3726fe48a92315ea3
ctorr148 <48769508+ctorr148@users.noreply.github.com>
2019-04-14 16:21:22 +0000

    rescorla wagner implementation
    rescorla wagner implementation
    new SmartStudent class
    reduced answers to questions to two

--- project1.py
+++ project1.py
@@ -8,7 +8,6 @@ import numpy as np
 np.random.seed(0)
 
 
-# useless stuff down there
 def main():
     learn()
 
@@ -18,14 +17,12 @@ class Teacher:
     def __init__(self):
         self.sheet = {1: 1, 2: 2, 3: 3, 4: 4, 5: 1, 6: 2, 7: 3, 8: 4}
 
-    # This can be a class method (i.e. @classmethod[new line]def ask(cls):[same as before]
     def ask(self):
         question = np.random.randint(low=1, high=9)
         return question
 
-    def answer_check(self, student, question, answer):
+    def answer_check(self, question, answer):
         if answer == self.sheet[question]:
-            # It would be better to this by calling a method (e.g., student.learn(question, answer) )
             student.memory[question] = answer
         else:
             pass
@@ -44,34 +41,27 @@ class Student:
             answer = self.memory[question]
         return answer
 
-# Create a SmartStudent class that inherit from student but is smarter by using RL
-
-# Reduce to two options
-
 
 def learn():
     epoch = 0
 
-    student = Student()
-    teacher = Teacher()
-
     print("training begins...")
 
-    # This is very beautiful... Another idea?
     while "a" in student.memory.values():
 
         question = teacher.ask()
         answer = student.think(question)
-        teacher.answer_check(student=student, question=question, answer=answer)
+        teacher.answer_check(question, answer)
 
         epoch += 1
-
-        # Consider using tqdm library
-        # plt graph
         print("epochs:", epoch)
 
     print("finished training")
     print(student.memory)
+
+
+student = Student()
+teacher = Teacher()
 
 
 if __name__ == "__main__":
